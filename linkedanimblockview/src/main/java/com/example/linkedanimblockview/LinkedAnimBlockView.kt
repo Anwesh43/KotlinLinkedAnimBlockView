3a84533ae089a6b28832c3a992b59e88ecd065d6
Anwesh43 <anweshthecool0@gmail.com>
2018-05-24 06:20:37 +0530

    adding LinkedAnimBlockView module

---
+++ linkedanimblockview/src/main/java/com/example/linkedanimblockview/LinkedAnimBlockView.kt
@@ -0,0 +1,10 @@
+package com.example.linkedanimblockview
+
+/**
+ * Created by anweshmishra on 24/05/18.
+ */
+
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.*
